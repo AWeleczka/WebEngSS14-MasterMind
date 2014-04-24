17d175f506c5bc0c98ece3cce7b078de43af897f
Alexander Weleczka <alex@warzonesecure.com>
2014-04-24 15:00:24 +0200

    Initial Commit

---
+++ mastermind.js
@@ -0,0 +1,124 @@
+/**
+ * MasterMind Clone written in HTML5 with JavaScript
+ * submission and PHP processing
+ *
+ * Clone of the commonly known game "Mastermind"
+ * Featuring a HTML5-Compliant userinterface built upon Bootstrap
+ * with jQuery-Based JavaScript-I/O-Processing
+ * and PHP Game-Logic.
+ * This Project was written as part of a practical exercise for the class
+ * "Web-Engeneering" in the Summer Semester 2014 at Hochschule RheinMain
+ *
+ * @author     Alexander Weleczka <mail@AWeleczka.de>
+ * @copyright  2014 Alexander Weleczka for WebEng006 SS14 HS-RM
+ * @version    1.0
+ */
+
+var current_attempt = {};
+
+function setColor(c) {
+  if(current_attempt.c1 == null) {
+    current_attempt.c1 = c;
+    updateGuess(c, 'guess_1');
+  } else if(current_attempt.c2 == null) {
+    current_attempt.c2 = c;
+    updateGuess(c, 'guess_2');
+  } else if(current_attempt.c3 == null) {
+    current_attempt.c3 = c;
+    updateGuess(c, 'guess_3');
+  } else if(current_attempt.c4 == null) {
+    current_attempt.c4 = c;
+    updateGuess(c, 'guess_4');
+
+    submitGuess();
+    resetGuess();
+    rebuildGame();
+  } else {
+    console.log("Den Zustand haben wir nicht ... versucht da jemand zu mogeln?");
+  }
+}
+
+function updateGuess(c, id) {
+  $('#' + id).addClass(c);
+  $('#' + id).removeClass('grey');
+}
+
+function resetGuess() {
+  current_attempt.c1 = null;
+  current_attempt.c2 = null;
+  current_attempt.c3 = null;
+  current_attempt.c4 = null;
+
+  $('[id^=guess_]').removeClass();
+  $('[id^=guess_]').addClass('col-md-3 btn grey');
+}
+
+function submitGuess() {
+  $.ajax({
+    url: './processor.php?guess',
+    type: "POST",
+    async: false,
+    data: current_attempt
+  }).done(function(data) {
+    current_attempt.data = data;
+  });
+}
+
+function restartGame() {
+  $.ajax({
+    url: './processor.php?reset',
+    async: false
+  }).done(function(data) {
+    current_attempt = {};
+    $('#thegame').html('<div class="row"><div class="col-md-12"><h3>Ich habe mir was neues ausgedacht</h3><p>Du hast 12 Versuche!</p></div></div>');
+    $('#remaining').html("12");
+    $('#colSel').show(250);
+  });
+}
+
+function rebuildGame() {
+  var json = $.parseJSON(current_attempt.data);
+  var html = "";
+
+  if(json.length < 12) {
+    $.each(json, function(i, it) {
+      html += '<div class="row">';
+      html += '    <div class="col-md-2 btn ' +it.c1+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c2+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c3+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c4+ '">&nbsp;</div>';
+      html += '    <div class="col-md-4">' +it.cMatch+ ' Farben<br/>' +it.pMatch+ ' Positionen</div>';
+      html += '</div>';
+    });
+    $('#remaining').html(12-json.length);
+
+    if(json[json.length-1].pMatch == 4) {
+      html += '<div class="row">';
+      html += '    <div class="col-md-8"><button class="btn btn-primary btn-block" onclick="restartGame();">Restart</button></div>';
+      html += '    <div class="col-md-4"><strong>WINNER</strong></div>';
+      html += '</div>';
+      $('#colSel').hide(250);
+    }
+  } else {
+    $.each(json, function(i, it) {
+      html += '<div class="row">';
+      html += '    <div class="col-md-2 btn ' +it.c1+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c2+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c3+ '">&nbsp;</div>';
+      html += '    <div class="col-md-2 btn ' +it.c4+ '">&nbsp;</div>';
+      html += '    <div class="col-md-4">' +it.cMatch+ ' Farben<br/>' +it.pMatch+ ' Positionen</div>';
+      html += '</div>';
+    });
+    
+    html += '<div class="row">';
+    html += '    <div class="col-md-8"><button class="btn btn-primary btn-block" onclick="restartGame();">Restart</button></div>';
+    html += '    <div class="col-md-4"><strong>Game Over</strong></div>';
+    html += '</div>';
+
+    $('#colSel').hide(250);
+  }
+  
+  $('#thegame').html(html);
+}
+
+//EOF
---
+++ processor.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * MasterMind Clone written in HTML5 with JavaScript
+ * submission and PHP processing
+ *
+ * Clone of the commonly known game "Mastermind"
+ * Featuring a HTML5-Compliant userinterface built upon Bootstrap
+ * with jQuery-Based JavaScript-I/O-Processing
+ * and PHP Game-Logic.
+ * This Project was written as part of a practical exercise for the class
+ * "Web-Engeneering" in the Summer Semester 2014 at Hochschule RheinMain
+ *
+ * @author     Alexander Weleczka <mail@AWeleczka.de>
+ * @copyright  2014 Alexander Weleczka for WebEng006 SS14 HS-RM
+ * @version    1.0
+ */
+
+session_start();
+
+/* Moegliche Farben */
+$colors = array('rot', 'gruen', 'blau', 'gelb', 'lila', 'teal');
+
+/*
+ * Initialisierung der Farbkombination, falls no keine in der Session existiers
+ */
+if(!isset($_SESSION['code'])) {
+
+    $solution = array();
+    for($i = 1; $i <= 4; $i++) {
+        $solution['c' . $i] = $colors[array_rand($colors)];
+    }
+    $_SESSION['code'] = json_encode($solution);
+
+}
+
+/*
+ * Spieler hat eine Kombination geraten, pruefe eingabe
+ */
+if(isset($_GET['guess'])) {
+
+    if(isset($_POST['data'])) {
+        $guesses = json_decode($_POST['data']);
+    } else {
+        $guesses = array();
+    }
+
+    $guess = array('c1' => $_POST['c1'], 'c2' => $_POST['c2'], 'c3' => $_POST['c3'], 'c4' => $_POST['c4']);
+    $original = json_decode($_SESSION['code'], TRUE);
+
+    /* Bestimme uebereinstimmende Positionen zwischen der Originalkombination und der Benutzereingabe */
+    $pMatch = 0;
+    foreach($guess as $k => $g) {
+        if($g == $original[$k]) {
+            $pMatch++;
+        }
+    }
+    $guess['pMatch'] = $pMatch;
+
+    /* Bestimme uebereinstimmende Farben zwischen der Originalkombination und der Benutzereingabe */
+    $cMatch = 0;
+    foreach($guess as $g) {
+        if($g != $pMatch) {
+            foreach($original as $k => $o) {
+                if($g == $o) {
+                    $cMatch++;
+                    $original[$k] = "undefined";
+                    break;
+                }
+            }
+        }
+    }
+    $guess['cMatch'] = $cMatch;
+
+    /* Entferne doppelte eintraege (Farbe an richtiger position zaehlt nur einmalig als Position) */
+    $guess['cMatch'] = $guess['cMatch'] - $guess['pMatch'];
+
+    array_push($guesses, $guess);
+    echo json_encode($guesses);
+
+/*
+ * Spieler moechte das Spiel neu starten - erzeuge neue Farbkombination
+ */
+} else if(isset($_GET['reset'])) {
+
+    $solution = array();
+    for($i = 1; $i <= 4; $i++) {
+        $solution['c' . $i] = $colors[array_rand($colors)];
+    }
+    $_SESSION['code'] = json_encode($solution);
+
+}
+
+//EOF
